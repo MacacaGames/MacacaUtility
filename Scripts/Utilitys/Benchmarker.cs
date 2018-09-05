788c2759010a6c644b4d596622d88bba97b0e2c2
Miki <sableangle@gmail.com>
2018-09-05 18:57:02 +0800

    Add Benchmark

---
+++ Scripts/Utilitys/Benchmarker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class Benchmarker : UnitySingleton<Benchmarker>
+{
+
+    static GameObject instanceGameObject;
+    public enum BenchMarkResult
+    {
+        High, Mid, Low, Faild
+    }
+    public IObservable<float> StarBenchmark(int sampleTime = 10)
+    {
+        return Observable.FromCoroutine<float>((observer, cancellationToken) => BenchMark(sampleTime, observer, cancellationToken));
+    }
+    IEnumerator BenchMark(int sampleTime, IObserver<float> observer, CancellationToken cancellationToken)
+    {
+        int frameCount = 0;
+
+        float StartTime = Time.time;
+        Debug.LogError("BenchMark Start");
+
+        while (!cancellationToken.IsCancellationRequested && Time.time - StartTime < sampleTime)
+        {
+            ++frameCount;
+            yield return null;
+        }
+        float avgFPS = frameCount / (float)sampleTime;
+        Debug.LogError("BenchMark Done");
+        Debug.LogError("平均FPS :" + avgFPS);
+        observer.OnNext(avgFPS);
+    }
+
+}
