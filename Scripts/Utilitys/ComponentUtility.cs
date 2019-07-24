68e1311275bbd21ea4688f914db11e3d1c0b6d4c
Owen <owen@cloudmacaca.com>
2019-07-24 22:11:05 +0800

    fix bug!!

--- Scripts/Utilitys/ComponentUtility.cs
+++ Scripts/Utilitys/ComponentUtility.cs
@@ -10,7 +10,7 @@ public static class ComponentUtility
     public static bool TryGetComponent<T>(this Component target, out T value)
     {
         value = target.GetComponent<T>();
-        return value == null;
+        return value != null;
     }
     /// <summary>
     /// 可嘗試取得Component，並回傳取得成功與否。out的value為取得的Component，若取得失敗則value為null。
@@ -18,7 +18,7 @@ public static class ComponentUtility
     public static bool TryGetComponent<T>(this GameObject target, out T value)
     {
         value = target.GetComponent<T>();
-        return value == null;
+        return value != null;
     }
 
     /// <summary>
@@ -27,7 +27,7 @@ public static class ComponentUtility
     public static bool TryGetComponent<T>(this Component target, out IEnumerable<T> values)
     {
         values = target.GetComponents<T>();
-        return values == null;
+        return values != null;
     }
     /// <summary>
     /// 可嘗試取得掛載的所有Components，並回傳取得成功與否。out的values為取得的Component，若取得失敗則value為null。
@@ -35,7 +35,7 @@ public static class ComponentUtility
     public static bool TryGetComponent<T>(this GameObject target, out IEnumerable<T> values)
     {
         values = target.GetComponents<T>();
-        return values == null;
+        return values != null;
     }
 
 }
