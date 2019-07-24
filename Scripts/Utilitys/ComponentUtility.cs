5c0df917c678cef59b433003558f41db6d3b43d5
Owen <owen@cloudmacaca.com>
2019-07-24 16:21:47 +0800

    CompnentUtility

---
+++ Scripts/Utilitys/ComponentUtility.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class ComponentUtility
+{
+
+    /// <summary>
+    /// 可嘗試取得Component，並回傳取得成功與否。out的value為取得的Component，若取得失敗則value為null。
+    /// </summary>
+    public static bool TryGetComponent<T>(this Component target, out T value)
+    {
+        value = target.GetComponent<T>();
+        return value == null;
+    }
+    /// <summary>
+    /// 可嘗試取得Component，並回傳取得成功與否。out的value為取得的Component，若取得失敗則value為null。
+    /// </summary>
+    public static bool TryGetComponent<T>(this GameObject target, out T value)
+    {
+        value = target.GetComponent<T>();
+        return value == null;
+    }
+
+    /// <summary>
+    /// 可嘗試取得掛載的所有Component，並回傳取得成功與否。out的values為取得的Component，若取得失敗則value為null。
+    /// </summary>
+    public static bool TryGetComponent<T>(this Component target, out IEnumerable<T> values)
+    {
+        values = target.GetComponents<T>();
+        return values == null;
+    }
+    /// <summary>
+    /// 可嘗試取得掛載的所有Components，並回傳取得成功與否。out的values為取得的Component，若取得失敗則value為null。
+    /// </summary>
+    public static bool TryGetComponent<T>(this GameObject target, out IEnumerable<T> values)
+    {
+        values = target.GetComponents<T>();
+        return values == null;
+    }
+
+}
