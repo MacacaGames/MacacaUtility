be69d311a15d664a3431e0029c9f85f2ae5518c2
miki <sableangle@gmail.com>
2019-08-13 12:20:12 +0800

    Add Inspect Target

--- Scripts/Editor/CMEditorUtility.cs
+++ Scripts/Editor/CMEditorUtility.cs
@@ -77,6 +77,31 @@ namespace CloudMacaca
         {
             return (int)imageData[offset] << 8 | (int)imageData[offset + 1];
         }
+        public static void InspectTarget(GameObject target)
+        {
+            // Get a reference to the `InspectorWindow` type object
+            var inspectorType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+            // Create an InspectorWindow instance
+            var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as UnityEditor.EditorWindow;
+            // We display it - currently, it will inspect whatever gameObject is currently selected
+            // So we need to find a way to let it inspect/aim at our target GO that we passed
+            // For that we do a simple trick:
+            // 1- Cache the current selected gameObject
+            // 2- Set the current selection to our target GO (so now all inspectors are targeting it)
+            // 3- Lock our created inspector to that target
+            // 4- Fallback to our previous selection
+            inspectorInstance.Show();
+            // Cache previous selected gameObject
+            var prevSelection = UnityEditor.Selection.activeGameObject;
+            // Set the selection to GO we want to inspect
+            UnityEditor.Selection.activeGameObject = target;
+            // Get a ref to the "locked" property, which will lock the state of the inspector to the current inspected target
+            var isLocked = inspectorType.GetProperty("isLocked", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+            // Invoke `isLocked` setter method passing 'true' to lock the inspector
+            isLocked.GetSetMethod().Invoke(inspectorInstance, new object[] { true });
+            // Finally revert back to the previous selection so that other inspectors continue to inspect whatever they were inspecting...
+            UnityEditor.Selection.activeGameObject = prevSelection;
+        }
     }
 
 }
