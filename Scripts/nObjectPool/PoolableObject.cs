607452b666bc9bec10de5285ca4b83d97155f551
miki <sableangle@gmail.com>
2020-11-05 17:53:11 +0800

    add nObjectPool from projectcube

---
+++ Scripts/nObjectPool/PoolableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolableObject : MonoBehaviour
+{
+    public int poolKey { get; private set; }
+
+    public void SetPoolKey(int key)
+    {
+        poolKey = key;
+    }
+
+    public virtual void RecoverSelf()
+    {
+        ObjectPoolManager.GetObjectPool(this).Recovery(this);
+    }
+    public virtual void RecoverSelf(float delay)
+    {
+        ObjectPoolManager.GetObjectPool(this).Recovery(this, delay);
+    }
+
+
+}
---
+++ Scripts/nObjectPool/nObjectPool.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class nObjectPool : MonoBehaviour
+{
+    private GameObject poolRoot;
+
+    [SerializeField]
+    private PoolableObject poolableObject;
+
+    public int poolKey { get; private set; }
+
+    [SerializeField]
+    private int initailSize = 3;
+    [SerializeField]
+    private Queue<PoolableObject> m_pool = new Queue<PoolableObject>();
+    [SerializeField]
+    private HashSet<PoolableObject> m_pool_using = new HashSet<PoolableObject>();
+
+    [SerializeField] bool autoInit = false;
+    [SerializeField] bool stayTransform = true;
+    void Awake()
+    {
+        if (autoInit)
+        {
+            Init(poolableObject, initailSize);
+        }
+    }
+
+    public void Init()
+    {
+        Init(poolableObject, initailSize);
+    }
+
+    public void Init(PoolableObject obj, int size)
+    {
+        poolableObject = obj;
+        initailSize = size;
+
+        poolKey = poolableObject.GetInstanceID();
+        poolRoot = new GameObject("Pool_" + obj.name + "_" + poolKey);
+        poolRoot.transform.SetParent(transform, stayTransform);
+        for (int cnt = 0; cnt < initailSize; cnt++)
+        {
+            PoolableObject newOne = NewOne();
+            m_pool.Enqueue(newOne);
+            newOne.gameObject.SetActive(false);
+            newOne.transform.SetParent(poolRoot.transform);
+        }
+    }
+
+    public PoolableObject ReUse(Vector3 position, Quaternion rotation)
+    {
+        PoolableObject poolableObject;
+        if (m_pool.Count > 0)
+            poolableObject = m_pool.Dequeue();
+        else
+            poolableObject = NewOne();
+
+        GameObject poolableObj = poolableObject.gameObject;
+        Transform poolableTransform = poolableObj.transform;
+        poolableTransform.SetPositionAndRotation(position, rotation);
+        poolableTransform.SetParent(poolRoot.transform);
+        poolableObj.SetActive(true);
+        m_pool_using.Add(poolableObject);
+
+        return poolableObject;
+    }
+
+    PoolableObject NewOne()
+    {
+        PoolableObject newOne = Instantiate(poolableObject) as PoolableObject;
+        newOne.gameObject.transform.SetParent(poolRoot.transform);
+        newOne.SetPoolKey(poolKey);
+        return newOne;
+    }
+
+    public T ReUse<T>(Vector3 position, Quaternion rotation)
+    {
+        PoolableObject poolableObject = ReUse(position,rotation);
+        GameObject poolableObj = poolableObject.gameObject;
+        return poolableObj.GetComponent<T>();
+    }
+
+    public void Recovery(PoolableObject poolableObject)
+    {
+        if (m_pool.Contains(poolableObject))
+            return;
+
+        GameObject poolable = poolableObject.gameObject;
+        m_pool.Enqueue(poolableObject);
+        poolable.SetActive(false);
+        poolable.transform.SetParent(poolRoot.transform);
+
+        if (m_pool_using.Contains(poolableObject))
+        {
+            m_pool_using.Remove(poolableObject);
+        }
+    }
+
+    public void Recovery(PoolableObject poolableObject, float sec)
+    {
+        StartCoroutine(DelayRecovery(poolableObject, sec));
+    }
+
+    IEnumerator DelayRecovery(PoolableObject poolableObject, float time)
+    {
+        yield return Yielders.GetWaitForSeconds(time);
+        Recovery(poolableObject);
+    }
+    public bool isReachPoolMax()
+    {
+        return m_pool.Count <= 0 ? true : false;
+    }
+
+    public void RecoveryAll()
+    {
+        foreach (PoolableObject poolableObject in m_pool_using)
+        {
+            if (poolableObject != null)
+            {
+                m_pool.Enqueue(poolableObject);
+                GameObject poolableObj = poolableObject.gameObject;
+                poolableObj.SetActive(false);
+                poolableObj.transform.SetParent(poolRoot.transform);
+            }
+            else
+            {
+                Debug.LogWarningFormat("{0} 池內物件發生未回收就被刪除的情況", poolRoot.name);
+            }
+        }
+        m_pool_using.Clear();
+    }
+
+    public int Count()
+    {
+        return m_pool.Count;
+    }
+}
