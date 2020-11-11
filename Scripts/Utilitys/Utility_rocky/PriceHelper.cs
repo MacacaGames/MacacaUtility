71e303de894895bf1ad4e200e3c9256b77e433ec
RockyHong <contact@rockyhong.com>
2020-11-11 19:36:14 +0800

    Price Helper

---
+++ Scripts/Utilitys/Utility_rocky/PriceHelper.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class PriceHelper
+{
+    /// <summary>
+    /// Get the price string mulptiple by times, times should bigger than 1
+    /// </summary>
+    /// <param name="originalPrice"></param>
+    /// <param name="times"></param>
+    /// <returns></returns>
+    static char[] numbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public static string GetTimesStringResultByPriceText(string originalPrice, float times)
+    {
+        int firstNumberIndex = originalPrice.IndexOfAny(numbers);
+        int lastNumberIndex = originalPrice.LastIndexOfAny(numbers);
+
+        string pre = originalPrice.Substring(0, firstNumberIndex - 1);
+        string last = originalPrice.Substring(lastNumberIndex + 1);
+
+        string midNumberString = originalPrice.Substring(firstNumberIndex, lastNumberIndex - firstNumberIndex + 1);
+
+        var midCharArray = midNumberString.ToCharArray();
+
+        //Reverse Array!!!
+        Array.Reverse(midCharArray);
+
+        List<int> numberList = new List<int>();
+        List <(int index, char character)> split = new List<(int index, char character)>();
+        for (int i = 0; i < midCharArray.Length; i++)
+        {
+            char c = midCharArray[i];
+            if (numbers.Contains(c))
+            {
+                numberList.Add(c - '0');
+            }
+            else
+                split.Add((i, c));
+        }
+
+        int number = 0;
+        for (int n = 0; n < numberList.Count; n++)
+        {
+            number += Mathf.RoundToInt(numberList[n] * Mathf.Pow(10, n));
+        }
+
+        //解析完畢 動工
+
+        number = Mathf.RoundToInt(number * times);
+
+        //反解析開始
+        char[] newNumberString = number.ToString().ToCharArray();
+        Array.Reverse(newNumberString);
+
+        string reversedNewNumberString = new string(newNumberString);
+
+        for (int i = 0; i < split.Count; i++)
+        {
+            int fixedIndex = split[i].index + i;
+            string insertChar = split[i].character.ToString();
+
+            reversedNewNumberString = reversedNewNumberString.Insert(fixedIndex, insertChar);
+        }
+
+        var s = reversedNewNumberString.ToArray();
+
+        Array.Reverse(s);
+
+        string s2 = new string(s);
+
+        return pre + s2 + last;
+    }
+}
---
+++ Scripts/Utilitys/Utility_rocky/PriceHelperTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class PriceHelperTester : MonoBehaviour
+{
+    [Button]
+    public void Test( string inputText,float times)
+    {
+        Debug.Log(
+            string.Format("inputText: {0}|  inputTimes: {1}|  output: {2}",
+            inputText,
+            times,
+            PriceHelper.GetTimesStringResultByPriceText(inputText, times)));
+    }
+}
