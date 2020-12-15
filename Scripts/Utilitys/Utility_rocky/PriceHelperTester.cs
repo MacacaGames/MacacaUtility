8393a8fe241b0427acbddae45c7cb2d26dae712c
miki <sableangle@gmail.com>
2020-12-15 15:24:55 +0800

    add package json remove dependency

--- Scripts/Utilitys/Utility_rocky/PriceHelperTester.cs
+++ Scripts/Utilitys/Utility_rocky/PriceHelperTester.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-using System.Linq;
-using Sirenix.OdinInspector;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using System;
+// using System.Linq;
+// using Sirenix.OdinInspector;
 
-public class PriceHelperTester:MonoBehaviour
-{
-    [SerializeField]
-    string originalPrice;
-    [SerializeField]
-    int times;
+// public class PriceHelperTester:MonoBehaviour
+// {
+//     [SerializeField]
+//     string originalPrice;
+//     [SerializeField]
+//     int times;
 
-    [Button]
-    public void Test()
-    {
-        var s = PriceHelper.GetTimesStringResultByPriceText(originalPrice, times);
-        Debug.Log(s);
-    }
-}
+//     [Button]
+//     public void Test()
+//     {
+//         var s = PriceHelper.GetTimesStringResultByPriceText(originalPrice, times);
+//         Debug.Log(s);
+//     }
+// }
