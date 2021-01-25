94a47d6e94805fd8329fd954b519ec425e6c86f3
miki <sableangle@gmail.com>
2021-01-25 15:29:31 +0800

    palette

---
+++ Scripts/Components/Palette/PaletteControl.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PaletteControl : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    [Header("Main Gradient Color")]
+    public Gradient gradient = null;
+
+    [Header("Shadow Image Color")]
+    public Image shadowImage = null;
+    public Shadow shadowComponent = null;
+
+    [SerializeField]
+    SelectablePalette palette;
+
+    [SerializeField]
+    Selectable selectable;
+    void Awake()
+    {
+        if (selectable == null)
+        {
+            selectable = GetComponent<Selectable>();
+        }
+    }
+
+    void Update()
+    {
+        if (lastSelectionState != currentSelectionState)
+        {
+            lastSelectionState = currentSelectionState;
+            OnSelectionStateChagne(currentSelectionState);
+        }
+    }
+
+    void OnSelectionStateChagne(SelectionState state)
+    {
+        SelectablePalette.PaletteSet paletteSet;
+        switch (state)
+        {
+            case SelectionState.Normal:
+                paletteSet = palette.GetNormalColor();
+                break;
+            case SelectionState.Highlighted:
+                paletteSet = palette.GetHighlightedColor();
+                break;
+            case SelectionState.Pressed:
+                paletteSet = palette.GetPressedColor();
+                break;
+            case SelectionState.Selected:
+                paletteSet = palette.GetSelectedColor();
+                break;
+            case SelectionState.Disabled:
+                paletteSet = palette.GetDisabledColor();
+                break;
+            default:
+                paletteSet = palette.GetNormalColor();
+                break;
+        }
+        palette.ChangeColor(paletteSet, shadowImage, shadowComponent, gradient);
+    }
+
+    SelectionState lastSelectionState;
+    protected SelectionState currentSelectionState
+    {
+        get
+        {
+            if (!selectable.IsInteractable())
+                return SelectionState.Disabled;
+            if (isPointerDown)
+                return SelectionState.Pressed;
+            if (hasSelection)
+                return SelectionState.Selected;
+            if (isPointerInside)
+                return SelectionState.Highlighted;
+            return SelectionState.Normal;
+        }
+    }
+
+    private bool isPointerInside { get; set; }
+    private bool isPointerDown { get; set; }
+    private bool hasSelection { get; set; }
+    void InstantClearState()
+    {
+        isPointerInside = false;
+        isPointerDown = false;
+        hasSelection = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPointerDown = false;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerInside = false;
+    }
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        isPointerInside = true;
+    }
+
+    void OnEnable()
+    {
+        isPointerDown = false;
+    }
+
+    void OnDisable()
+    {
+        InstantClearState();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        hasSelection = false;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        hasSelection = true;
+    }
+
+    /// <summary>
+    /// An enumeration of selected states of objects
+    /// </summary>
+    protected enum SelectionState
+    {
+        /// <summary>
+        /// The UI object can be selected.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// The UI object is highlighted.
+        /// </summary>
+        Highlighted,
+
+        /// <summary>
+        /// The UI object is pressed.
+        /// </summary>
+        Pressed,
+
+        /// <summary>
+        /// The UI object is selected
+        /// </summary>
+        Selected,
+
+        /// <summary>
+        /// The UI object cannot be selected.
+        /// </summary>
+        Disabled,
+    }
+}
---
+++ Scripts/Components/Palette/SelectablePalette.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+[CreateAssetMenu]
+public class SelectablePalette : ScriptableObject
+{
+    public Color addHighlightedColor = new Color(0.05f, 0.05f, 0.05f, 0f);
+    public Color addPressedColor = new Color(0.1f, 0.1f, 0.1f, 0f);
+
+    const string Main_Gradient_Color = "Main Gradient Color";
+
+    [Header(Main_Gradient_Color)]
+#if ODIN_INSPECTOR
+    [FoldoutGroup(Main_Gradient_Color)]
+#endif
+    public Color normalColorV1;
+
+#if ODIN_INSPECTOR
+    [FoldoutGroup(Main_Gradient_Color)]
+#endif
+    public Color normalColorV2;
+
+#if ODIN_INSPECTOR
+    [FoldoutGroup(Main_Gradient_Color)]
+#endif
+    public Color disabledColorV1;
+
+#if ODIN_INSPECTOR
+    [FoldoutGroup(Main_Gradient_Color)]
+#endif
+    public Color disabledColorV2;
+
+    public Color highlightedColorV1
+    {
+        get
+        {
+            return normalColorV1 + addHighlightedColor;
+        }
+    }
+    public Color highlightedColorV2
+    {
+        get
+        {
+            return normalColorV2 + addHighlightedColor;
+        }
+    }
+    public Color pressedColorV1
+    {
+        get
+        {
+            return normalColorV1 + addPressedColor;
+        }
+    }
+    public Color pressedColorV2
+    {
+        get
+        {
+            return normalColorV2 + addPressedColor;
+        }
+    }
+    public Color selectedColorV1
+    {
+        get
+        {
+            return highlightedColorV1;
+        }
+    }
+    public Color selectedColorV2
+    {
+        get
+        {
+            return highlightedColorV2;
+        }
+    }
+
+    const string Shadow_Image_Color = "Shadow Image Color";
+#if ODIN_INSPECTOR
+    [FoldoutGroup(Shadow_Image_Color)]
+#endif
+    public Color shadowNormalColor;
+
+#if ODIN_INSPECTOR
+    [FoldoutGroup(Shadow_Image_Color)]
+#endif
+    public Color shadowDisabledColor;
+
+    public Color shadowHighlightedColor
+    {
+        get
+        {
+            return shadowNormalColor + addHighlightedColor;
+
+        }
+    }
+    public Color shadowPressedColor
+    {
+        get
+        {
+            return shadowNormalColor + addPressedColor;
+
+        }
+    }
+    public Color shadowSelectedColor
+    {
+        get
+        {
+            return shadowHighlightedColor;
+        }
+    }
+
+
+    public struct PaletteSet
+    {
+        public Color shadowColor;
+        public Color color1;
+        public Color color2;
+    }
+
+    public PaletteSet GetNormalColor()
+    {
+        return new PaletteSet
+        {
+            shadowColor = shadowNormalColor,
+            color1 = normalColorV1,
+            color2 = normalColorV2
+        };
+    }
+
+    public PaletteSet GetHighlightedColor()
+    {
+        return new PaletteSet
+        {
+            shadowColor = shadowHighlightedColor,
+            color1 = highlightedColorV1,
+            color2 = highlightedColorV2
+        };
+    }
+
+    public PaletteSet GetPressedColor()
+    {
+        return new PaletteSet
+        {
+            shadowColor = shadowPressedColor,
+            color1 = pressedColorV1,
+            color2 = pressedColorV2
+        };
+    }
+
+    public PaletteSet GetSelectedColor()
+    {
+        return new PaletteSet
+        {
+            shadowColor = shadowSelectedColor,
+            color1 = selectedColorV1,
+            color2 = selectedColorV2
+        };
+    }
+
+    public PaletteSet GetDisabledColor()
+    {
+        return new PaletteSet
+        {
+            shadowColor = shadowDisabledColor,
+            color1 = disabledColorV1,
+            color2 = disabledColorV2
+        };
+    }
+    public void ChangeColor(PaletteSet paletteSet, Image shadowImage, Shadow shadowComponent, Gradient mainGradientComponent)
+    {
+        if (shadowImage != null & shadowImage?.color != paletteSet.shadowColor) shadowImage.color = paletteSet.shadowColor;
+        if (shadowComponent != null & shadowComponent?.effectColor != paletteSet.shadowColor) shadowComponent.effectColor = paletteSet.shadowColor;
+        if (mainGradientComponent != null)
+        {
+            if (mainGradientComponent.Vertex1 != paletteSet.color1) mainGradientComponent.Vertex1 = paletteSet.color1;
+            if (mainGradientComponent.Vertex2 != paletteSet.color2) mainGradientComponent.Vertex2 = paletteSet.color2;
+        }
+#if UNITY_EDITOR
+        if (mainGradientComponent) UnityEditor.PrefabUtility.RecordPrefabInstancePropertyModifications(mainGradientComponent);
+        if (shadowComponent) UnityEditor.PrefabUtility.RecordPrefabInstancePropertyModifications(shadowComponent);
+        if (shadowImage) UnityEditor.PrefabUtility.RecordPrefabInstancePropertyModifications(shadowImage);
+#endif
+    }
+}
