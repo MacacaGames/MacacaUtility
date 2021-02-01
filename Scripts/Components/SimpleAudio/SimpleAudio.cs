c8a0c5f55a8cb80f8b60cbc433547e4bde0ccc92
miki <sableangle@gmail.com>
2021-02-01 17:28:56 +0800

    Simple audio namespace, version 1.0.4

--- Scripts/Components/SimpleAudio/SimpleAudio.cs
+++ Scripts/Components/SimpleAudio/SimpleAudio.cs
@@ -3,282 +3,285 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-public class SimpleAudio : MonoBehaviour
+namespace MacacaGames
 {
-    static SimpleAudio _instance;
-    public static SimpleAudio Instance
+    public class SimpleAudio : MonoBehaviour
     {
-        get
+        static SimpleAudio _instance;
+        public static SimpleAudio Instance
         {
-            if (_instance == null)
-                _instance = Initiate();
-            return _instance;
-        }
-    }
-
-    static AudioMixer mixer;
-    static AudioMixerGroup musicMixerGroup;
-    static AudioMixerGroup lobbyMixerGroup;
-    static AudioMixerGroup sfxMixerGroup;
-    static AudioMixerGroup voiceMixerGroup;
-
-    static SimpleAudio Initiate()
-    {
-        GameObject host = new GameObject();
-        host.name = "SimpleAudio";
-        var ins = host.AddComponent<SimpleAudio>();
-        DontDestroyOnLoad(host);
-
-        mixer = Resources.Load<AudioMixer>("SimpleAudio/Mixer");
-
-        if (mixer)
-        {
-            sfxMixerGroup = mixer.FindMatchingGroups("Sfx")[0];
-            voiceMixerGroup = mixer.FindMatchingGroups("Voice")[0];
-            musicMixerGroup = mixer.FindMatchingGroups("Music")[0];
-            lobbyMixerGroup = mixer.FindMatchingGroups("LobbyMusic")[0];
-            mixer.GetFloat("Sfx", out oriVolumeSFX);
-            mixer.GetFloat("Voice", out oriVolumeVoice);
-            mixer.GetFloat("Music", out oriVolumeMusic);
-            mixer.GetFloat("LobbyMusic", out oriVolumeLobby);
+            get
+            {
+                if (_instance == null)
+                    _instance = Initiate();
+                return _instance;
+            }
         }
 
-        ins.ApplyMusicState();
-        ins.ApplySFXState();
-        return ins;
-    }
+        static AudioMixer mixer;
+        static AudioMixerGroup musicMixerGroup;
+        static AudioMixerGroup lobbyMixerGroup;
+        static AudioMixerGroup sfxMixerGroup;
+        static AudioMixerGroup voiceMixerGroup;
 
-    const string PP_IsMusicMute = "IsMusicMute";
-    const string PP_IsSfxMute = "PP_IsSfxMute";
-    public bool IsMusicMute
-    {
-        get
+        static SimpleAudio Initiate()
         {
-            return PlayerPrefs.GetInt(PP_IsMusicMute, 0) == 1;
+            GameObject host = new GameObject();
+            host.name = "SimpleAudio";
+            var ins = host.AddComponent<SimpleAudio>();
+            DontDestroyOnLoad(host);
+
+            mixer = Resources.Load<AudioMixer>("SimpleAudio/Mixer");
+
+            if (mixer)
+            {
+                sfxMixerGroup = mixer.FindMatchingGroups("Sfx")[0];
+                voiceMixerGroup = mixer.FindMatchingGroups("Voice")[0];
+                musicMixerGroup = mixer.FindMatchingGroups("Music")[0];
+                lobbyMixerGroup = mixer.FindMatchingGroups("LobbyMusic")[0];
+                mixer.GetFloat("Sfx", out oriVolumeSFX);
+                mixer.GetFloat("Voice", out oriVolumeVoice);
+                mixer.GetFloat("Music", out oriVolumeMusic);
+                mixer.GetFloat("LobbyMusic", out oriVolumeLobby);
+            }
+
+            ins.ApplyMusicState();
+            ins.ApplySFXState();
+            return ins;
         }
-        set
+
+        const string PP_IsMusicMute = "IsMusicMute";
+        const string PP_IsSfxMute = "PP_IsSfxMute";
+        public bool IsMusicMute
         {
-            PlayerPrefs.SetInt(PP_IsMusicMute, value ? 1 : 0);
+            get
+            {
+                return PlayerPrefs.GetInt(PP_IsMusicMute, 0) == 1;
+            }
+            set
+            {
+                PlayerPrefs.SetInt(PP_IsMusicMute, value ? 1 : 0);
+                ApplyMusicState();
+            }
+        }
+
+        public bool IsSfxMute
+        {
+            get
+            {
+                return PlayerPrefs.GetInt(PP_IsSfxMute, 0) == 1;
+            }
+            set
+            {
+                PlayerPrefs.SetInt(PP_IsSfxMute, value ? 1 : 0);
+                ApplySFXState();
+            }
+        }
+
+        public void ApplyMusicState()
+        {
+            if (mixer)
+            {
+                if (!IsMusicMute)
+                {
+                    mixer.SetFloat("Music", oriVolumeMusic);
+                    mixer.SetFloat("LobbyMusic", oriVolumeLobby);
+                }
+                else
+                {
+                    mixer.SetFloat("Music", -80);
+                    mixer.SetFloat("LobbyMusic", -80);
+                }
+            }
+            else
+            {
+                if (!IsMusicMute)
+                {
+                    AudioListener.volume = 1;
+                }
+                else
+                {
+                    AudioListener.volume = 0;
+                }
+            }
+        }
+
+        public void ApplySFXState()
+        {
+            if (mixer)
+            {
+                if (!IsSfxMute)
+                {
+                    mixer.SetFloat("Sfx", oriVolumeSFX);
+                    mixer.SetFloat("Voice", oriVolumeVoice);
+                }
+                else
+                {
+                    mixer.SetFloat("Sfx", -80);
+                    mixer.SetFloat("Voice", -80);
+                }
+            }
+            else
+            {
+                if (IsSfxMute)
+                {
+                    AudioListener.volume = 1;
+                }
+                else
+                {
+                    AudioListener.volume = 0;
+                }
+            }
+        }
+
+
+        public enum AudioOutput { Sfx, Voice }
+
+        static float oriVolumeSFX, oriVolumeVoice, oriVolumeMusic, oriVolumeLobby;
+
+        /// <summary>
+        /// AudioMixer setfloat not work in awake()
+        /// </summary>
+        void Start()
+        {
             ApplyMusicState();
-        }
-    }
-
-    public bool IsSfxMute
-    {
-        get
-        {
-            return PlayerPrefs.GetInt(PP_IsSfxMute, 0) == 1;
-        }
-        set
-        {
-            PlayerPrefs.SetInt(PP_IsSfxMute, value ? 1 : 0);
             ApplySFXState();
         }
-    }
 
-    public void ApplyMusicState()
-    {
-        if (mixer)
+
+        public void StopMusic()
         {
-            if (!IsMusicMute)
+            GetMusicAudioSource().Stop();
+        }
+
+        public void PlayMusic(AudioClip clip)
+        {
+            GetMusicAudioSource().clip = clip;
+            GetMusicAudioSource().time = 0;
+            GetMusicAudioSource().Play();
+            if (GetLobbyMusicAudioSource().isPlaying)
             {
-                mixer.SetFloat("Music", oriVolumeMusic);
-                mixer.SetFloat("LobbyMusic", oriVolumeLobby);
+                GetLobbyMusicAudioSource().Stop();
             }
+        }
+
+        public void PlayMusicPreview(AudioClip clip)
+        {
+            GetMusicAudioSource().clip = clip;
+            GetMusicAudioSource().time = 10f;
+            GetMusicAudioSource().Play();
+            if (GetLobbyMusicAudioSource().isPlaying)
+            {
+                GetLobbyMusicAudioSource().Stop();
+            }
+        }
+
+        public void PlayLobbyMusic(AudioClip clip)
+        {
+            GetLobbyMusicAudioSource().clip = clip;
+            GetLobbyMusicAudioSource().Play();
+        }
+
+        public bool IsMusicPlaying()
+        {
+            return GetMusicAudioSource().isPlaying;
+            // return false;
+        }
+
+        public bool IsMusicPlaying(AudioClip clip)
+        {
+            return GetMusicAudioSource().isPlaying && GetMusicAudioSource().clip == clip;
+            // return false;
+        }
+
+        public float GetMusicProgress()
+        {
+            if (GetMusicAudioSource().clip == null)
+            {
+                return 0;
+            }
+            return GetMusicAudioSource().time / GetMusicAudioSource().clip.length;
+        }
+
+        public float GetMusicPlayTime()
+        {
+            return GetMusicAudioSource().time;
+        }
+
+        Queue<AudioSource> sources = new Queue<AudioSource>();
+        AudioSource GetAvailableSource()
+        {
+            AudioSource result = null;
+            if (sources.Count > 0)
+                result = sources.Dequeue();
             else
             {
-                mixer.SetFloat("Music", -80);
-                mixer.SetFloat("LobbyMusic", -80);
+                result = gameObject.AddComponent<AudioSource>();
             }
+            return result;
         }
-        else
+
+        AudioSource _musicAudioSource = null;
+        AudioSource GetMusicAudioSource()
         {
-            if (!IsMusicMute)
+            if (_musicAudioSource == null)
             {
-                AudioListener.volume = 1;
+                _musicAudioSource = gameObject.AddComponent<AudioSource>();
+                _musicAudioSource.outputAudioMixerGroup = musicMixerGroup;
             }
-            else
+            return _musicAudioSource;
+        }
+        AudioSource _lobbyMusicAudioSource = null;
+        AudioSource GetLobbyMusicAudioSource()
+        {
+            if (_lobbyMusicAudioSource == null)
             {
-                AudioListener.volume = 0;
+                _lobbyMusicAudioSource = gameObject.AddComponent<AudioSource>();
+                _lobbyMusicAudioSource.outputAudioMixerGroup = lobbyMixerGroup;
+                _lobbyMusicAudioSource.loop = true;
             }
+            return _lobbyMusicAudioSource;
         }
-    }
 
-    public void ApplySFXState()
-    {
-        if (mixer)
+        /// <summary>
+        /// Play sfx
+        /// </summary>
+        /// <param name="clip"></param>
+        /// <param name="output"></param>
+        public void PlaySFX(AudioClip clip, AudioOutput output = AudioOutput.Sfx)
         {
-            if (!IsSfxMute)
-            {
-                mixer.SetFloat("Sfx", oriVolumeSFX);
-                mixer.SetFloat("Voice", oriVolumeVoice);
-            }
-            else
-            {
-                mixer.SetFloat("Sfx", -80);
-                mixer.SetFloat("Voice", -80);
-            }
+            if (clip == null)
+                return;
+            StartCoroutine(SFXManager(clip, output));
         }
-        else
+
+        /// <summary>
+        /// Play sfx from on of the clips
+        /// </summary>
+        /// <param name="clips">clips</param>
+        /// <param name="output"></param>
+        public void PlaySFX(AudioClip[] clips, AudioOutput output = AudioOutput.Sfx)
         {
-            if (IsSfxMute)
-            {
-                AudioListener.volume = 1;
-            }
-            else
-            {
-                AudioListener.volume = 0;
-            }
+            PlaySFX(clips[Random.Range(0, clips.Length)], output);
         }
-    }
 
-
-    public enum AudioOutput { Sfx, Voice }
-
-    static float oriVolumeSFX, oriVolumeVoice, oriVolumeMusic, oriVolumeLobby;
-
-    /// <summary>
-    /// AudioMixer setfloat not work in awake()
-    /// </summary>
-    void Start()
-    {
-        ApplyMusicState();
-        ApplySFXState();
-    }
-
-
-    public void StopMusic()
-    {
-        GetMusicAudioSource().Stop();
-    }
-
-    public void PlayMusic(AudioClip clip)
-    {
-        GetMusicAudioSource().clip = clip;
-        GetMusicAudioSource().time = 0;
-        GetMusicAudioSource().Play();
-        if (GetLobbyMusicAudioSource().isPlaying)
+        IEnumerator SFXManager(AudioClip clip, AudioOutput output = AudioOutput.Sfx)
         {
-            GetLobbyMusicAudioSource().Stop();
+            AudioSource source = GetAvailableSource();
+            source.enabled = true;
+            source.clip = clip;
+            source.outputAudioMixerGroup = sfxMixerGroup;
+
+            if (output == AudioOutput.Voice)
+                source.outputAudioMixerGroup = voiceMixerGroup;
+
+            source.Play();
+            while (source.isPlaying)
+                yield return null;
+
+            source.clip = null;
+            source.enabled = false;
+            sources.Enqueue(source);
         }
-    }
-
-    public void PlayMusicPreview(AudioClip clip)
-    {
-        GetMusicAudioSource().clip = clip;
-        GetMusicAudioSource().time = 10f;
-        GetMusicAudioSource().Play();
-        if (GetLobbyMusicAudioSource().isPlaying)
-        {
-            GetLobbyMusicAudioSource().Stop();
-        }
-    }
-
-    public void PlayLobbyMusic(AudioClip clip)
-    {
-        GetLobbyMusicAudioSource().clip = clip;
-        GetLobbyMusicAudioSource().Play();
-    }
-
-    public bool IsMusicPlaying()
-    {
-        return GetMusicAudioSource().isPlaying;
-        // return false;
-    }
-
-    public bool IsMusicPlaying(AudioClip clip)
-    {
-        return GetMusicAudioSource().isPlaying && GetMusicAudioSource().clip == clip;
-        // return false;
-    }
-
-    public float GetMusicProgress()
-    {
-        if (GetMusicAudioSource().clip == null)
-        {
-            return 0;
-        }
-        return GetMusicAudioSource().time / GetMusicAudioSource().clip.length;
-    }
-
-    public float GetMusicPlayTime()
-    {
-        return GetMusicAudioSource().time;
-    }
-
-    Queue<AudioSource> sources = new Queue<AudioSource>();
-    AudioSource GetAvailableSource()
-    {
-        AudioSource result = null;
-        if (sources.Count > 0)
-            result = sources.Dequeue();
-        else
-        {
-            result = gameObject.AddComponent<AudioSource>();
-        }
-        return result;
-    }
-
-    AudioSource _musicAudioSource = null;
-    AudioSource GetMusicAudioSource()
-    {
-        if (_musicAudioSource == null)
-        {
-            _musicAudioSource = gameObject.AddComponent<AudioSource>();
-            _musicAudioSource.outputAudioMixerGroup = musicMixerGroup;
-        }
-        return _musicAudioSource;
-    }
-    AudioSource _lobbyMusicAudioSource = null;
-    AudioSource GetLobbyMusicAudioSource()
-    {
-        if (_lobbyMusicAudioSource == null)
-        {
-            _lobbyMusicAudioSource = gameObject.AddComponent<AudioSource>();
-            _lobbyMusicAudioSource.outputAudioMixerGroup = lobbyMixerGroup;
-            _lobbyMusicAudioSource.loop = true;
-        }
-        return _lobbyMusicAudioSource;
-    }
-
-    /// <summary>
-    /// Play sfx
-    /// </summary>
-    /// <param name="clip"></param>
-    /// <param name="output"></param>
-    public void PlaySFX(AudioClip clip, AudioOutput output = AudioOutput.Sfx)
-    {
-        if (clip == null)
-            return;
-        StartCoroutine(SFXManager(clip, output));
-    }
-
-    /// <summary>
-    /// Play sfx from on of the clips
-    /// </summary>
-    /// <param name="clips">clips</param>
-    /// <param name="output"></param>
-    public void PlaySFX(AudioClip[] clips, AudioOutput output = AudioOutput.Sfx)
-    {
-        PlaySFX(clips[Random.Range(0, clips.Length)], output);
-    }
-
-    IEnumerator SFXManager(AudioClip clip, AudioOutput output = AudioOutput.Sfx)
-    {
-        AudioSource source = GetAvailableSource();
-        source.enabled = true;
-        source.clip = clip;
-        source.outputAudioMixerGroup = sfxMixerGroup;
-
-        if (output == AudioOutput.Voice)
-            source.outputAudioMixerGroup = voiceMixerGroup;
-
-        source.Play();
-        while (source.isPlaying)
-            yield return null;
-
-        source.clip = null;
-        source.enabled = false;
-        sources.Enqueue(source);
     }
 }
--- Scripts/Components/SimpleAudio/SimpleSFXPlayer.cs
+++ Scripts/Components/SimpleAudio/SimpleSFXPlayer.cs
@@ -4,150 +4,153 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.EventSystems;
 
-public class SimpleSFXPlayer : UIBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IScrollHandler
+namespace MacacaGames
 {
+    public class SimpleSFXPlayer : UIBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IScrollHandler
+    {
 
-    [SerializeField]
-    private bool m_Interactable = true;
-    private bool m_SelectableInteractable
-    {
-        get
+        [SerializeField]
+        private bool m_Interactable = true;
+        private bool m_SelectableInteractable
         {
-            if (m_SelectableCache != null)
+            get
             {
-                return m_SelectableCache.interactable;
-            }
-            return true;
-        }
-    }
-    private bool m_GroupsAllowInteraction = true;
-    private readonly List<CanvasGroup> m_CanvasGroupCache = new List<CanvasGroup>();
-    private UnityEngine.UI.Selectable m_SelectableCache;
-    protected override void OnCanvasGroupChanged()
-    {
-        // Figure out if parent groups allow interaction
-        // If no interaction is alowed... then we need
-        // to not do that :)
-        var groupAllowInteraction = true;
-        Transform t = transform;
-        while (t != null)
-        {
-            t.GetComponents(m_CanvasGroupCache);
-            bool shouldBreak = false;
-            for (var i = 0; i < m_CanvasGroupCache.Count; i++)
-            {
-                // if the parent group does not allow interaction
-                // we need to break
-                if (!m_CanvasGroupCache[i].interactable)
+                if (m_SelectableCache != null)
                 {
-                    groupAllowInteraction = false;
-                    shouldBreak = true;
+                    return m_SelectableCache.interactable;
                 }
-                // if this is a 'fresh' group, then break
-                // as we should not consider parents
-                if (m_CanvasGroupCache[i].ignoreParentGroups)
-                    shouldBreak = true;
+                return true;
             }
-            if (shouldBreak)
-                break;
-
-            t = t.parent;
         }
-
-        if (groupAllowInteraction != m_GroupsAllowInteraction)
+        private bool m_GroupsAllowInteraction = true;
+        private readonly List<CanvasGroup> m_CanvasGroupCache = new List<CanvasGroup>();
+        private UnityEngine.UI.Selectable m_SelectableCache;
+        protected override void OnCanvasGroupChanged()
         {
-            m_GroupsAllowInteraction = groupAllowInteraction;
-        }
-    }
+            // Figure out if parent groups allow interaction
+            // If no interaction is alowed... then we need
+            // to not do that :)
+            var groupAllowInteraction = true;
+            Transform t = transform;
+            while (t != null)
+            {
+                t.GetComponents(m_CanvasGroupCache);
+                bool shouldBreak = false;
+                for (var i = 0; i < m_CanvasGroupCache.Count; i++)
+                {
+                    // if the parent group does not allow interaction
+                    // we need to break
+                    if (!m_CanvasGroupCache[i].interactable)
+                    {
+                        groupAllowInteraction = false;
+                        shouldBreak = true;
+                    }
+                    // if this is a 'fresh' group, then break
+                    // as we should not consider parents
+                    if (m_CanvasGroupCache[i].ignoreParentGroups)
+                        shouldBreak = true;
+                }
+                if (shouldBreak)
+                    break;
 
-    public virtual bool IsInteractable
-    {
-        get
+                t = t.parent;
+            }
+
+            if (groupAllowInteraction != m_GroupsAllowInteraction)
+            {
+                m_GroupsAllowInteraction = groupAllowInteraction;
+            }
+        }
+
+        public virtual bool IsInteractable
         {
-            return m_GroupsAllowInteraction && m_Interactable && m_SelectableInteractable;
+            get
+            {
+                return m_GroupsAllowInteraction && m_Interactable && m_SelectableInteractable;
+            }
         }
-    }
 
 
-    [System.Serializable]
-    public enum PlayTiming
-    {
-        OnEnable,
-        OnDisable,
-        OnHoverEnter,
-        OnHoverExit,
-        OnClickUp,
-        OnClickDown,
-        OnScroll,   // Mouse scroll
-        DontPlay,
-    }
-    [System.Serializable]
-    public class PlayerModule
-    {
-        public PlayTiming playTiming;
-        public AudioClip clip;
-    }
-    PlayerModule _currentPlayerModule;
-    [Tooltip("playOnAwake")]
-    public bool playOnAwake = false;
-    public PlayerModule[] playerModules;
-    new void Start()
-    {
-        playOnAwake = true;
-        m_SelectableCache = GetComponent<UnityEngine.UI.Selectable>();
-    }
+        [System.Serializable]
+        public enum PlayTiming
+        {
+            OnEnable,
+            OnDisable,
+            OnHoverEnter,
+            OnHoverExit,
+            OnClickUp,
+            OnClickDown,
+            OnScroll,   // Mouse scroll
+            DontPlay,
+        }
+        [System.Serializable]
+        public class PlayerModule
+        {
+            public PlayTiming playTiming;
+            public AudioClip clip;
+        }
+        PlayerModule _currentPlayerModule;
+        [Tooltip("playOnAwake")]
+        public bool playOnAwake = false;
+        public PlayerModule[] playerModules;
+        new void Start()
+        {
+            playOnAwake = true;
+            m_SelectableCache = GetComponent<UnityEngine.UI.Selectable>();
+        }
 
 
-    void OnEnable()
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnEnable);
-        Play();
-    }
+        void OnEnable()
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnEnable);
+            Play();
+        }
 
-    void OnDisable()
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnDisable);
-        Play();
-    }
+        void OnDisable()
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnDisable);
+            Play();
+        }
 
-    public virtual void OnPointerEnter(PointerEventData eventData)
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnHoverEnter);
-        Play();
-    }
+        public virtual void OnPointerEnter(PointerEventData eventData)
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnHoverEnter);
+            Play();
+        }
 
-    public virtual void OnPointerExit(PointerEventData eventData)
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnHoverExit);
-        Play();
-    }
+        public virtual void OnPointerExit(PointerEventData eventData)
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnHoverExit);
+            Play();
+        }
 
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnClickDown);
-        Play();
-    }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnClickDown);
+            Play();
+        }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnClickUp);
-        Play();
-    }
-    public void OnScroll(PointerEventData eventData)
-    {
-        _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnScroll);
-        Play();
-    }
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnClickUp);
+            Play();
+        }
+        public void OnScroll(PointerEventData eventData)
+        {
+            _currentPlayerModule = playerModules.FirstOrDefault(m => m.playTiming == PlayTiming.OnScroll);
+            Play();
+        }
 
-    public void Play()
-    {
-        if (IsInteractable == false) return;
-        if (_currentPlayerModule == null) return;
-        if (playOnAwake == false) return;
-        OnPlaySfx?.Invoke(_currentPlayerModule.clip);
-        _currentPlayerModule = null;
-    }
+        public void Play()
+        {
+            if (IsInteractable == false) return;
+            if (_currentPlayerModule == null) return;
+            if (playOnAwake == false) return;
+            OnPlaySfx?.Invoke(_currentPlayerModule.clip);
+            _currentPlayerModule = null;
+        }
 
-    public static System.Action<AudioClip> OnPlaySfx;
+        public static System.Action<AudioClip> OnPlaySfx;
+    }
 }
