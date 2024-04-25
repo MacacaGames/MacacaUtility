c360d8b256f36fc2fb7f58cfd9299a0671b9deff
oldman <oldman@macaca.onmicrosoft.com>
2024-04-25 16:22:20 +0800

    feat: add new poolableVfx implementation

---
+++ Scripts/Components/PoolablePlayableDirector.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof (PlayableDirector))]
+public class PoolablePlayableDirector : PoolableVfx
+{
+    PlayableDirector _playableDirector;
+    protected PlayableDirector playableDirector
+    {
+        get
+        {
+            if (_playableDirector == null)
+            {
+                _playableDirector = GetComponent<PlayableDirector>();
+            }
+            return _playableDirector;
+        }
+    }
+
+    protected override void StopVfx()
+    {
+        playableDirector.Stop();
+    }
+
+    protected override void PlayVfx()
+    {
+        playableDirector.Play();
+    }
+
+    protected override bool IsVfxPlaying()
+    {
+        return playableDirector.state == PlayState.Playing;
+    }
+}
